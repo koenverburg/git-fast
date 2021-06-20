63f18816e73125abe98dca5845dc58c3a4ac37d5
Koen Verburg <creativekoen@gmail.com>
2021-06-20 13:12:41 +0200

    chore(git): updated git ignore file

---
+++ src/go.mod
@@ -0,0 +1,10 @@
+module github.com/koenverburg/git-fast
+
+go 1.16
+
+require (
+	github.com/go-git/go-git v4.7.0+incompatible // indirect
+	github.com/go-git/go-git/v5 v5.4.2 // indirect
+	github.com/manifoldco/promptui v0.8.0 // indirect
+	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5"
+	"github.com/manifoldco/promptui"
+)
+
+// @Summary CheckIfError
+// @Description
+// @Param  eError
+func CheckIfError(e error) {
+  if e != nil {
+    log.Fatal(e)
+    panic(e)
+  }
+}
+
+func filterEmptyString(s []string ) []string {
+  var cleaned []string
+	for _, str := range s {
+    if str != "" {
+      cleaned = append(cleaned, str)
+    }
+	}
+	return cleaned
+}
+
+func getUntrackedFiles() ([]string, git.Worktree) {
+  dir, err := filepath.Abs(filepath.Dir(os.Args[1]))
+  CheckIfError(err)
+  fmt.Println(fmt.Sprint("folder %s", dir))
+
+  repo, err := git.PlainOpen(dir)
+  CheckIfError(err)
+
+  worktree, err := repo.Worktree()
+  CheckIfError(err)
+
+  status, err := worktree.Status()
+  CheckIfError(err)
+
+  s1 := strings.ReplaceAll(status.String(), "?? ", "")
+  s2 := strings.ReplaceAll(s1, "A  ", "")
+  s3 := strings.ReplaceAll(s2, " M ", "")
+
+  return strings.Split(s3, "\n"), *worktree
+}
+
+func genericSelectPrompt(label string, items []string) string {
+  prompt := promptui.Select{
+		Label: label,
+    Items: filterEmptyString(append(items, "none")),
+  }
+
+	_, result, err := prompt.Run()
+  CheckIfError(err)
+
+  return result
+}
+
+func showUntrackedList(files []string) string {
+  prompt := promptui.Select{
+		Label: "Select files to commit",
+		Items: filterEmptyString(append(files, "done")),
+	}
+
+	_, result, err := prompt.Run()
+  CheckIfError(err)
+
+  return result
+}
+
+func stageSelectedFiles(files []string, worktree git.Worktree) {
+  for _, v := range files {
+    worktree.Add(v)
+  }
+}
+
+func genericInputPrompt(label string) string {
+  validate := func(input string) error {
+    if len(input) == 0 {
+      return errors.New("no input given")
+    }
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    label,
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+  CheckIfError(err)
+
+  return result
+}
+
+type Segment struct {
+  value string
+  part string
+}
+
+func isEmpty(str string) bool {
+  if str == "" {
+    return true
+  }
+  return false
+}
+
+func useSegment(value string) string {
+  if !isEmpty(value) && value != "none" {
+    return value
+  }
+  return ""
+}
+
+func includeInMessage(segments []Segment) string {
+  var sb string
+  for _, segment := range segments {
+    switch {
+    case segment.part == "type":
+      sb += useSegment(segment.value)
+
+    case segment.part == "scope":
+      if !isEmpty(segment.value) {
+        sb += fmt.Sprintf("(%s): ", segment.value)
+      }
+
+    case segment.part == "commit":
+      sb += useSegment(segment.value)
+      sb += " "
+
+    case segment.part == "tag":
+      sb += useSegment(segment.value)
+      sb += " "
+
+    case segment.part == "ticket":
+      if !isEmpty(segment.value) && segment.value != "x" {
+        sb += fmt.Sprintf("#%s ", segment.value)
+      }
+    }
+  }
+  return sb
+}
+
+func createSegment(value string, part string) Segment {
+  var s Segment
+  s.value = value
+  s.part = part
+  return s
+}
+
+func commitMessageWizard() string {
+  typeString := genericSelectPrompt("Select the type of change", []string{
+      "feat",
+      "fix",
+      "docs",
+      "style",
+      "refactor",
+      "perf",
+      "test",
+      "chore",
+    },
+  )
+  typeSegement := createSegment(typeString, "type")
+
+  scope := genericInputPrompt("Scope")
+  scopeSegement := createSegment(scope, "scope")
+
+  commit := genericInputPrompt("Commit message")
+  commitSegement := createSegment(commit, "commit")
+
+  tags := genericSelectPrompt("Select tags", []string{
+      "WIP",
+      "[skip ci]",
+    },
+  )
+  tagSegement := createSegment(tags, "tag")
+
+  ticket := genericInputPrompt("ticket number")
+  ticketSegement := createSegment(ticket, "ticket")
+
+  result := includeInMessage([]Segment{
+    typeSegement,
+    scopeSegement,
+    commitSegement,
+    ticketSegement,
+    tagSegement,
+  })
+
+  return result 
+}
+
+func main() {
+  files, worktree := getUntrackedFiles()
+
+  var selection []string
+
+  for {
+    selected := showUntrackedList(files)
+    if selected == "done" {
+      break
+    } else {
+      selection = append(selection, selected)
+    }
+  }
+
+  stageSelectedFiles(files, worktree)
+
+  msg := commitMessageWizard()
+
+  _, commitErr := worktree.Commit(msg, &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "Koen Verburg",
+			Email: "creativekoen@gmail.com",
+			When:  time.Now(),
+		},
+	})
+  CheckIfError(commitErr)
+
+  fmt.Println(msg)
+}
